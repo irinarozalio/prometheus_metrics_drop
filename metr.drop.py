14ccb38af0b4ddb2cc30d62886e5d3d3914a1249
Irina Rozalio <irinarozalio@gmail.com>
2019-06-12 19:40:06 +0000

    first commit

--- metr.drop.py
+++ metr.drop.py
@@ -53,8 +53,9 @@ if __name__ == "__main__":
 	filename = "/home/irinarozalio/prometheus_metrics_drop/prometheus/prometheus.yml"
 	targets = prom_yam_target(filename)
 
-	prom_list = {}
+	
 	for i in targets:
+		prom_list = {}
 		prom_target = str(i).split("'")[1]
 		tunix_5 = time.mktime((datetime.datetime.now() - datetime.timedelta(minutes=24*60)).timetuple())
 		tunix_now = time.mktime(datetime.datetime.now().timetuple())
@@ -63,20 +64,20 @@ if __name__ == "__main__":
 		response_now = requests.get(prometheus + '/api/v1/query',
 			params={
 					'query': instance,
-					'time': tunix_now})
-
-		
+					'time': tunix_now})		
 		results_now = response_now.json()['data']['result']
 		response_5 = requests.get(prometheus + '/api/v1/query',
 			params={
 					'query': instance,
 					'time': tunix_5})
+		# print(response_5.json())
 		# print(response_5.url)
+		# quit()
 		results_5 = response_5.json()['data']['result']
 
 		print("******************************")
 		print("Starting Target=" + prom_target)
-		print(response_now.url)
+		# print(response_now.url)
 
 		# prom_list = {}
 		for result_now in results_now:
@@ -84,21 +85,26 @@ if __name__ == "__main__":
 			metric_values = int(result_now['value'][1])
 			prom_list[metric_name] = {'now': metric_values, 'last_5': None}
 
-		for result_now in results_5:
-			metric_name = result_now['metric']['__name__']
-			metric_values = int(result_now['value'][1])
+		# print(results_5)
+		for result_5 in results_5:
+			metric_name = result_5['metric']['__name__']
+			# print(metric_name)
+			metric_values = int(result_5['value'][1])
 			if metric_name in prom_list.keys():
 				prom_list[metric_name]['last_5'] = metric_values
-		
+		# quit()
+		# pprint.pprint(prom_list)
 		for k, v in prom_list.items():
 			if v['now'] != v['last_5'] and v['last_5'] and v['now']:
 				perc = abs(float(((v['now'] - v['last_5']) * 100 ) / v['now']))				
 				if perc > 30:
-					print(perc)
+					# print(perc)
 					print(k, v)
-					prom_target = '35.222.74.15:5000'
-					metric_name='fake_vendetta'
-					change_prom_yaml(filename,metric_name,prom_target)
+					#print(prom_list[metric_name])
+					# prom_target = '35.222.74.15:5000'
+					# metric_name='fake_aborting'
+					change_prom_yaml(filename,k,prom_target)
+		# quit()
 	prom_reload(prometheus)
 	
 	
