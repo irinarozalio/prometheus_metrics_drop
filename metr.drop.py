d73fad99d698f018620cb2bc4d2da72192e21e3a
Irina Rozalio <irinarozalio@gmail.com>
2019-06-12 10:56:44 +0000

    first commit

--- metr.drop.py
+++ metr.drop.py
@@ -4,20 +4,9 @@ import requests
 import sys
 from ruamel.yaml import YAML
 import pprint
-import datetime
-import time
-import requests
 
-# def yaml_loader(filepath):
-# 	with open(filename,"r") as file_descriptor:
-# 		data = yaml.load(file_descriptor)
-# 	return data
 
-# def yaml_dump(filepath,data):
-# 	with open(filename,"w") as file_descriptor:
-# 		yaml.dump(data, file_descriptor)
-
-def change_prom_yaml(filepath,metric_name):
+def change_prom_yaml(filepath,metric_name,prom_target):
 	yaml = YAML()
 	# data = yaml_loader(filepath)
 	with open(filename,"r") as file_descriptor:
@@ -25,20 +14,31 @@ def change_prom_yaml(filepath,metric_name):
 	#yaml.dump(data, sys.stdout)
 
 	for i in data['scrape_configs']:
-		# pprint.pprint(i)
-		if 'metric_relabel_configs' in i.keys():
-			if (metric_name not in i['metric_relabel_configs'][0]['regex']):
-				pprint.pprint("YYY=" + i['metric_relabel_configs'][0]['regex'])
-				i['metric_relabel_configs'].append(dict(source_labels=[ '__name__' ],regex=metric_name,action='drop'))
+		targ = i['static_configs'][0]['targets']
+		if prom_target in targ:
+			if 'metric_relabel_configs' in i.keys():
+				if ( metric_name not in i['metric_relabel_configs'][0]['regex']):
+					i['metric_relabel_configs'].append(dict(source_labels=[ '__name__' ],regex=metric_name,action='drop'))
+				else:
+					print('Metric ' + metric_name + " already exist.")
 			else:
-				print('Metric ' + metric_name + " already exist.")
-		else:
-			i['metric_relabel_configs'] = [dict(source_labels=[ '__name__' ],regex=metric_name,action='drop')]
+				i['metric_relabel_configs'] = [dict(source_labels=[ '__name__' ],regex=metric_name,action='drop')]
 
-	with open(filename, "w") as file:
-		yaml.indent(mapping=2)
-		yaml.dump(data, file)
-		yaml.dump(data, sys.stdout)
+			with open(filename, "w") as file:
+				yaml.indent(mapping=2)
+				yaml.dump(data, file)
+				# yaml.dump(data, sys.stdout)
+
+def prom_yam_target(filepath):
+	yaml = YAML()
+	targets = []
+	with open(filename,"r") as file_descriptor:
+		data = yaml.load(file_descriptor)
+	for i in data['scrape_configs']: 
+		target = i['static_configs'][0]['targets']
+		targets.append(i['static_configs'][0]['targets'])
+	return(targets)
+
 
 def prom_reload(prometheus):
 	query = prometheus + '-/reload'
@@ -51,38 +51,54 @@ def prom_reload(prometheus):
 if __name__ == "__main__":
 	prometheus = 'http://localhost:9090/'
 	filename = "/home/irinarozalio/prometheus_metrics_drop/prometheus/prometheus.yml"
+	targets = prom_yam_target(filename)
 
-	end_of_month = datetime.datetime.today().replace(day=1).date()
-	ts = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-	tz_5 = (datetime.datetime.now() - datetime.timedelta(minutes=5)).strftime('%Y-%m-%dT%H:%M:%SZ')
-	tz_now = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
-	tunix_5 = time.mktime((datetime.datetime.now() - datetime.timedelta(minutes=5)).timetuple())
-	tunix_now = time.mktime(datetime.datetime.now().timetuple())
+	prom_list = {}
+	for i in targets:
+		prom_target = str(i).split("'")[1]
+		tunix_5 = time.mktime((datetime.datetime.now() - datetime.timedelta(minutes=24*60)).timetuple())
+		tunix_now = time.mktime(datetime.datetime.now().timetuple())
 
-	response_now = requests.get(prometheus + '/api/v1/query',
-		params={
-				'query': 'topk(10, count by (__name__)({__name__=~".+"}))',
-				'time': tunix_now})
-	results_now = response_now.json()['data']['result']
+		instance = 'count({instance="' + prom_target + '"})  by  (__name__)'
+		response_now = requests.get(prometheus + '/api/v1/query',
+			params={
+					'query': instance,
+					'time': tunix_now})
 
-	response_5 = requests.get(prometheus + '/api/v1/query',
-		params={
-				'query': 'topk(10, count by (__name__)({__name__=~".+"}))',
-				'time': tunix_5})
-	results_5 = response_5.json()['data']['result']
+		
+		results_now = response_now.json()['data']['result']
+		response_5 = requests.get(prometheus + '/api/v1/query',
+			params={
+					'query': instance,
+					'time': tunix_5})
+		# print(response_5.url)
+		results_5 = response_5.json()['data']['result']
 
-	for result_5 in results_5:
+		print("******************************")
+		print("Starting Target=" + prom_target)
+		print(response_now.url)
+
+		# prom_list = {}
 		for result_now in results_now:
-			metric_name_5 = result_5['metric']['__name__']
-			metric_values_5 = int(result_5['value'][1])
 			metric_name = result_now['metric']['__name__']
 			metric_values = int(result_now['value'][1])
-			p = abs(float(100 - ((metric_values_5 * 100)/metric_values)))
-			if (metric_name_5 == metric_name) and (p < 0.6):
-				print(p)
-				print(metric_name, metric_values)
-	metric_name="irinarozalio1"
-	#d = dict(metric_relabel_configs=[dict(source_labels='[ __name__ ]',regex='my_too_large_metric',action='drop')])
-	change_prom_yaml(filename,metric_name)
+			prom_list[metric_name] = {'now': metric_values, 'last_5': None}
+
+		for result_now in results_5:
+			metric_name = result_now['metric']['__name__']
+			metric_values = int(result_now['value'][1])
+			if metric_name in prom_list.keys():
+				prom_list[metric_name]['last_5'] = metric_values
+		
+		for k, v in prom_list.items():
+			if v['now'] != v['last_5'] and v['last_5'] and v['now']:
+				perc = abs(float(((v['now'] - v['last_5']) * 100 ) / v['now']))				
+				if perc > 30:
+					print(perc)
+					print(k, v)
+					prom_target = '35.222.74.15:5000'
+					metric_name='fake_vendetta'
+					change_prom_yaml(filename,metric_name,prom_target)
 	prom_reload(prometheus)
+	
 	
